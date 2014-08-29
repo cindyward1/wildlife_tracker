3ff3b3cb373d28cd927502be4619aa7d27b6aa62
Cindy Ward <student@epicodus.com>
2014-08-29 13:49:29 -0700

    CRUD/L sightings, CRUD/L species

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,23 +1,52 @@
 require "date"
 
 class SightingsController < ApplicationController
+
   def index
     @today = Date.today
     render('sightings/index.html.erb')
   end
 
   def create
-    @sighting = Sighting.new(params[:sighting])
+    @today = Date.today
     @specie = nil
-    @specie = Specie.find(:name=>params[:sighting[:name]])
-    if @specie == nil
-      @specie = Specie.new(:name=>params[:sighting[:name]])
+    @specie = Specie.where(:name=>params[:specie_name]).first
+    if @specie.nil?
+      @specie = Specie.create(:name=>params[:specie_name])
     end
-    @sighting.specie_id = @specie.id
-    if @specie.save
+    params[:sighting][:specie_id] = @specie.id
+    p params[:sighting]
+    @sighting = Sighting.new(params[:sighting])
+    if @sighting.save
       render('sightings/success.html.erb')
     else
       render('sightings/index.html.erb')
     end
   end
+
+  def all
+    @sightings = Sighting.all.order(:date)
+    render('sightings/all.html.erb')
+  end
+
+  def edit
+    @sighting = Sighting.find(params[:id])
+    render('sightings/edit.html.erb')
+  end
+
+  def update
+    @sighting = Sighting.find(params[:id])
+    if @sighting.update(params[:sighting])
+      render('sightings/updated.html.erb')
+    else
+      render('sightings/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @sighting = Sighting.find(params[:id])
+    @sighting.destroy
+    render('sightings/destroyed.html.erb')
+  end
+
 end
---
+++ app/controllers/species_controller.rb
@@ -0,0 +1,46 @@
+class SpeciesController < ApplicationController
+
+  def index
+    @species = Specie.all.order(:name)
+    render('/species/index.html.erb')
+  end
+
+  def new
+    @specie = Specie.new
+    render('species/new.html.erb')
+  end
+
+  def create
+    @specie = Specie.new(:name=>params[:name])
+    if @specie.save
+      render('species/success.html.erb')
+    else
+      render('species/new.html.erb')
+    end
+  end
+
+  def edit
+    @specie = Specie.find(params[:id])
+    @sightings = @specie.sightings.where(:specie_id=>@specie.id)
+    render('species/edit.html.erb')
+  end
+
+  def update
+    @specie = Specie.find(params[:id])
+    if @specie.update(params[:name])
+      render('species/updated.html.erb')
+    else
+      render('species/edit.html.erb')
+    end
+  end
+
+  def destroy
+    @specie = Specie.find(params[:id])
+    @sightings = @specie.sightings.where(:specie_id=>@specie.id)
+    @sightings.each do |sighting|
+      sighting.destroy
+    end
+    @specie.destroy
+    render('species/destroyed.html.erb')
+  end
+end
